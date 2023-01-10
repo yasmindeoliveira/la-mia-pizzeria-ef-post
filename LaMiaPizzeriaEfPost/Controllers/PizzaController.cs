a8e7101ed47d36b966f6f8fe98c2754455c1ee3f
yasmindeoliveira <yaz.oliveira01@gmail.com>
2023-01-10 15:00:23 +0100

    Add project files.

---
+++ LaMiaPizzeriaEfPost/Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using LaMiaPizzeriaEfPost.Models;
+using LaMiaPizzeriaEfPost.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace LaMiaPizzeriaModel.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza>pizzas= PizzaData.GetPizzas();
+
+            return View("Index", pizzas);
+        }
+
+        public IActionResult Dettagli(string nome)
+        {
+            List<Pizza> pizzas = PizzaData.GetPizzas();
+
+            foreach (Pizza pizza in pizzas)
+            {
+                if (pizza.nome == nome)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("La pizza con il nome cercato non è disponibile");
+        }
+
+    }
+}
---
+++ LaMiaPizzeriaEfPost/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaMiaPizzeriaEfPost.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
